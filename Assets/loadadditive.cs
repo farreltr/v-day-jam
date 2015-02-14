8567c9f1b762723fd4915783fd0cf3ac0f10dd13
Claudio Medina <missthetarget@gmail.com>
2015-02-14 12:01:38 +0000

    Scene Manager v 0.1

---
+++ Assets/loadadditive.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadadditive : MonoBehaviour {
+
+    int numberRooms = 2;
+    int x;
+    GameObject Scene;
+	// Use this for initialization
+	void Start () {
+        for (x = 1; x <= numberRooms; x++)
+        {
+
+            Application.LoadLevelAdditive(x);   
+        }
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
